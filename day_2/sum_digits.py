8942f3fbd62a51755f82cd5ad2d7a4d87b2f4352
Euster <euster2agamara@gmail.com>
2016-04-26 17:59:47 -0700

    This is my sum

---
+++ day_2/sum_digits.py
@@ -0,0 +1,21 @@
+def sum_digits(A):
+	'''
+	takes a list A, and returns 
+	the sum of all the digits in the 
+	list e.g. [10, 30, 45] should return 1 + 0 + 3 + 0+4 +5
+	'''
+	total = 0
+
+	for i in A:
+		g = str(i)
+		for x in g:
+			v = int(x)
+			total += v
+	return total
+su
+
+
+
+# test tour code
+
+print sum_digits([10, 30, 45])
