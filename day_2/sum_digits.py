8e10bc3189d6f2e667dd4146091606b3596127b2
Euster <euster2agamara@gmail.com>
2016-04-27 16:54:02 -0700

    Programs for day 3

--- day_2/sum_digits.py
+++ day_2/sum_digits.py
@@ -1,19 +1,17 @@
 def sum_digits(A):
-	'''
-	takes a list A, and returns 
-	the sum of all the digits in the 
-	list e.g. [10, 30, 45] should return 1 + 0 + 3 + 0+4 +5
-	'''
-	total = 0
+    '''
+    takes a list A, and returns
+    the sum of all the digits in the
+    list e.g. [10, 30, 45] should return 1 + 0 + 3 + 0+4 +5
+    '''
+    total = 0
 
-	for i in A:
-		g = str(i)
-		for x in g:
-			v = int(x)
-			total += v
-	return total
-su
-
+    for i in A:
+        g = str(i)
+        for x in g:
+            v = int(x)
+            total += v
+    return total
 
 
 # test tour code
--- funky.py
+++ funky.py
@@ -1,26 +1,2 @@
-def funky (a,b):
-	'''concanate data types'''
-	# if type (a) and type (b) == str:
-	# 	print (a+b)
-	# elif type(a) and type(b) == int or type(a) and type (b) == float:
-	# 	return sum
-	if type(a) == str and type(b) == str:
-		print (a + b)
-	elif (type(a) == int or type(a) == float) or (type(b) == int or type(b) == float):
-		print (a+b)
-	elif type(a) == list and type(b) == list:
-		print (a+b)
-	elif type(a) == dict and type(b) == dict:
-		return (a,b)
-	else:
-		print "None"
-
-
-funky([10,20],[1.5,6])
-funky(1,2.4)
-funky("Euster", "japheth")
-funky({"name": 20, "age": 39}, {"height": 68})
-
-
-
-# test code
+def function():
+	pass
